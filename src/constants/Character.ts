b5ada4a85ab2f7905e921e189db2393f7b0af20b
Downeel <XarchangeldennX@gmail.com>
2024-01-26 02:57:45 +0300

    Some kind of shitt 2

--- src/constants/Character.ts
+++ src/constants/Character.ts
@@ -124,11 +124,11 @@ export const SKILL_MODIFIERS: SkillModifiers = {
 		criticalMultiplier: [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]
 	},
 	stealth: {
-		evadeChance: [0.02, 0.04, 0.06, 0.08, 0.1, 0.12, 0.14, 0.16, 0.18, 0.2]
-			criticalMultiplier: [0.025, 0.05, 0.075, 0.1, 0.125, 0.15, 0.175, 0.2, 0.225, 0.25]Z
+		evadeChance: [0.02, 0.04, 0.06, 0.08, 0.1, 0.12, 0.14, 0.16, 0.18, 0.2],
+		criticalMultiplier: [0.025, 0.05, 0.075, 0.1, 0.125, 0.15, 0.175, 0.2, 0.225, 0.25]
 	},
 	shooting: {
-				rangeDamageMultiplier: [0.1, 0.2, 0.3, 0.4, 0.5, 0.65, 0.7, 0.85, 1.0, 1.25],
+		rangeDamageMultiplier: [0.1, 0.2, 0.3, 0.4, 0.5, 0.65, 0.7, 0.85, 1.0, 1.25],
 		criticalMultiplier: [0.025, 0.05, 0.075, 0.1, 0.125, 0.15, 0.2, 0.25, 0.3, 0.35]
 	},
 	constitution: {
