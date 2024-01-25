e9cfdfa71177a76a20a60e50e1df9c79f674e27e
Downeel <XarchangeldennX@gmail.com>
2024-01-26 02:56:11 +0300

    Some kind of shittt

--- src/constants/Character.ts
+++ src/constants/Character.ts
@@ -113,7 +113,7 @@ export const SKILL_MODIFIERS: SkillModifiers = {
 	},
 	fight: {
 		meleeDamageMultiplier: [0.1, 0.2, 0.3, 0.4, 0.5, 0.65, 0.7, 0.85, 1.0, 1.25],
-		criticalMultiplier: [0.025, 0.05, 0.075, 0.1, 0.125, 0.15, 0.2, 0.25, 0.3, 0.35]
+		additionalMaxHealth: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 	},
 	knowledge: {
 		criticalChance: [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1]
@@ -125,9 +125,10 @@ export const SKILL_MODIFIERS: SkillModifiers = {
 	},
 	stealth: {
 		evadeChance: [0.02, 0.04, 0.06, 0.08, 0.1, 0.12, 0.14, 0.16, 0.18, 0.2]
+			criticalMultiplier: [0.025, 0.05, 0.075, 0.1, 0.125, 0.15, 0.175, 0.2, 0.225, 0.25]Z
 	},
 	shooting: {
-		rangeDamageMultiplier: [0.1, 0.2, 0.3, 0.4, 0.5, 0.65, 0.7, 0.85, 1.0, 1.25],
+				rangeDamageMultiplier: [0.1, 0.2, 0.3, 0.4, 0.5, 0.65, 0.7, 0.85, 1.0, 1.25],
 		criticalMultiplier: [0.025, 0.05, 0.075, 0.1, 0.125, 0.15, 0.2, 0.25, 0.3, 0.35]
 	},
 	constitution: {
@@ -154,16 +155,16 @@ export enum BodyPart {
 	Head,
 	Jaws,
 	Eyes,
-	Neck,
+		Neck,
 	Shoulder,
 	UpperArm,
 	Elbow,
 	LowerArm,
 	Wrist,
 	Fingers,
-	Chest,
+		Chest,
 	Stomach,
-	Groin,
+		Groin,
 	UpperLeg,
 	Knee,
 	LowerLeg,
